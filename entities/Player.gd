22d22df8f73b477b49c64005bd0caf853ebdcf05
regen=Q <keannyooi@gmail.com>
2024-04-05 12:55:10 +0800

    Rough implementation of swing and bounce mechanic done

--- entities/Player.gd
+++ entities/Player.gd
@@ -1,20 +1,33 @@
 extends Area2D
 
-const speed: int = 300
+const speed: int = 400
 
-@onready var collision_shape_2d = $CollisionShape2D
+@onready var hitbox := $Hitbox
+@onready var swing_collision_area := $SwingCollisionArea
+@onready var yinyang_orb := $"../YinyangOrb"
 
-var viewport_size: Vector2i
 var player_size: Vector2i
+var viewport_size: Vector2i
 
 func _ready() -> void:
+	player_size = hitbox.shape.get_rect().size
 	viewport_size = get_viewport().size
-	player_size = collision_shape_2d.shape.get_rect().size
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var input_map: float = Input.get_axis("player_left", "player_right")
 	handle_movement(input_map, delta)
+	
+	if (Input.is_action_just_pressed("player_swing")
+	and swing_collision_area.has_overlapping_bodies()):
+		yinyang_orb.handle_player_swing(position)
+	
+	if has_overlapping_bodies():
+		die()
+		
 	update_animations()
+
+func die() -> void:
+	print("oofs")
 
 func handle_movement(input_map: float, delta: float) -> void:
 	position.x += input_map * speed * delta
@@ -25,4 +38,6 @@ func handle_movement(input_map: float, delta: float) -> void:
 	)
 
 func update_animations() -> void:
+	#if velocity.x != 0:
+		#move animation
 	pass
--- entities/YinyangOrb.gd
+++ entities/YinyangOrb.gd
@@ -1,16 +1,27 @@
 extends CharacterBody2D
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var swing_bounce: float = 500.0
+
+@onready var sprite_2d = $Sprite2D
+
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
-	velocity.x = 200
+	velocity = Vector2(-500, -500)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
-	
 	var collision: KinematicCollision2D = (
 		move_and_collide(velocity * delta)
 	)
 	
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
+		velocity.y *= 0.9
+	
+	sprite_2d.flip_h = velocity.x < 0
+
+func handle_player_swing(player_pos: Vector2) -> void:
+	var pos_diff: Vector2 = position - player_pos
+	
+	velocity = Vector2(swing_bounce * (pos_diff.x / 50), swing_bounce)
